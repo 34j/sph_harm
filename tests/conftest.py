5ba1dd5b495459966090e7c29687fbea0a2586fc
34j <34j.github@proton.me>
2025-08-25 10:40:01 +0900

    test: wip

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from array_api._2024_12 import ArrayNamespaceFull
 
 
-@pytest.fixture(scope="session", params=["numpy", "torch"])
+@pytest.fixture(scope="session", params=["numpy"])
 def xp(request: pytest.FixtureRequest) -> ArrayNamespaceFull:
     """Get the array namespace for the given backend."""
     backend = request.param
